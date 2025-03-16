23ce4001b4ca65e7b26cc06a368c32bc953a448b
Natural_Horse <1648848391@qq.com>
2025-03-16 10:49:53 +0800

    删了多余的东西（shell,两个参数监测,还有uav_ws,这几个不是虚拟机上有就是实机上有）

--- shell/easy/2-test_rplidar.sh
+++
@@ -1,5 +0,0 @@
-source /opt/ros/melodic/setup.bash
-source ~/uav_ws/devel/setup.bash
-#####test connection with rplidar#####
-gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
---tab -e  'bash -c "sleep 5; roslaunch rplidar_ros view_rplidar.launch; exec bash"' \
